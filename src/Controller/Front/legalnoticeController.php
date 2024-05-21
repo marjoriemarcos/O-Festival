86f832032e26d62f3d6553a48c076ca71e9f1742
Nicolas Joubert <nicolasjoubert@icloud.com>
2024-05-21 17:42:02 +0200

    page legalnotice on twig

---
+++ src/Controller/Front/legalnoticeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class legalnoticeController extends AbstractController
+{
+    #[Route('/cgv', name: 'legalNoticeBrowse')]
+    public function browse(): Response
+    {
+        return $this->render('front/legalnotice/browse.html.twig', [
+            'controller_name' => 'legalnoticeController',
+        ]);
+    }
+}
