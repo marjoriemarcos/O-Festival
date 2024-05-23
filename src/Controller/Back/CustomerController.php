7f4173d8476e395e2290c38906f8cd5f332ef4f8
choulakbadri <choulakbadri@gmail.com>
2024-05-23 14:11:55 +0200

    back customer fixtures integrated

--- src/Controller/Back/CustomerController.php
+++ src/Controller/Back/CustomerController.php
@@ -5,22 +5,34 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CustomerRepository;
 
 class CustomerController extends AbstractController
 {
     #[Route('/back/customer_list', name: 'customerBrowse')]
-    public function customerBrowse(): Response
+    public function customerBrowse(CustomerRepository $customerRepository): Response
     {
+        // Fetch customers
+        $customerList = $customerRepository->findAll();
+        
         return $this->render('back/customer/index.html.twig', [
-            'controller_name' => 'CustomerController',
+            'customerList' => $customerList,
         ]);
     }
 
-    #[Route('/back/customer_list/read', name: 'customerRead')]
-    public function customerRead(): Response
+    #[Route('/back/customer_list/{id}', name: 'customerRead')]
+    public function customerRead(int $id, CustomerRepository $customerRepository): Response
     {
+        // Fetch the customer by its ID
+        $customer = $customerRepository->find($id);
+        
+        // Check if the customer exists
+        if (!$customer) {
+            throw $this->createNotFoundException('The customer does not exist.');
+        }
+
         return $this->render('back/customer/read.html.twig', [
-            'controller_name' => 'CustomerController',
+            'customer' => $customer,
         ]);
     }
 }
