de21b53b481e10afe0cd925432c9d6d3d0841a03
choulakbadri <choulakbadri@gmail.com>
2024-05-23 13:38:30 +0200

    back artist fixtures integrated

--- src/Controller/Back/ArtistController.php
+++ src/Controller/Back/ArtistController.php
@@ -5,22 +5,35 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ArtistRepository;
 
 class ArtistController extends AbstractController
 {
     #[Route('/back/artist_list', name: 'artistBackBrowse')]
-    public function artistBrowse(): Response
+    public function artistBrowse(ArtistRepository $artistRepository): Response
     {
+        // Fetch artists
+        $artistList = $artistRepository->findAll();
+
         return $this->render('back/artist/index.html.twig', [
-            'controller_name' => 'ArtistController',
+            'artistList' => $artistList,
         ]);
     }
 
-    #[Route('/back/artist_list/read', name: 'artistBackRead')]
-    public function artistRead(): Response
+    #[Route('/back/artist_list/{id}', name: 'artistBackRead')]
+    public function artistRead(int $id, ArtistRepository $artistRepository): Response
     {
+        // Fetch the artist by its ID
+        $artist = $artistRepository->find($id);
+
+        // Check if the artist exists
+        if (!$artist) {
+            throw $this->createNotFoundException('The artist does not exist.');
+        }
+
+        // Pass the artist to the view
         return $this->render('back/artist/read.html.twig', [
-            'controller_name' => 'ArtistController',
+            'artist' => $artist,
         ]);
     }
 }
