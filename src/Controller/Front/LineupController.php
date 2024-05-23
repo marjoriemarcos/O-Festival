1a25c41deeff46fbce83534eb5fae39b6db14a06
Marjorie MARCOS <marjorie.marcos@oclock.school>
2024-05-23 16:32:08 +0200

    first step on sort by day

--- src/Controller/Front/LineupController.php
+++ src/Controller/Front/LineupController.php
@@ -6,16 +6,49 @@ use App\Entity\Artist;
 use App\Entity\Slot;
 use App\Repository\SlotRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LineupController extends AbstractController
 {
     #[Route('/programmation', name: 'lineupBrowse', methods: ['GET'])]
-    public function lineupBrowse(SlotRepository $slotRepository): Response
+    public function lineupBrowse(SlotRepository $slotRepository, Request $request): Response
     {   
-        $lineUpListList = $slotRepository->findAllSlotWithArtistAndGenre();
-        dump($lineUpListList);
+        $lineUpListList = $slotRepository->findAllSlotWithArtistAndGenreAndStage();
+        return $this->render('front/lineup/browse.html.twig', [
+            'lineUpListList' => $lineUpListList,
+        ]);
+    }
+
+    #[Route('/programmation/{id}', name: 'LineUpByDate', methods: ['GET'])]
+    public function lineUpByDate(SlotRepository $slotRepository, $id): Response
+    {   
+        $days = [
+            'J1' => '2024-08-23',
+            'J2' => '2024-08-24',
+            'J3' => '2024-08-25',
+        ];
+
+        if ($id == 'J0') {
+            $lineUpListList = $slotRepository->findAllSlotWithArtistAndGenreAndStage();
+            if (!array_key_exists($id, $days)) {
+                        throw $this->createNotFoundException('Le jour spécifié est invalide.');
+            }
+        }
+        $date = $days[$id];
+        $lineUpListList = $slotRepository->findAllSlotWithArtistAndGenreAndStageByDay($date);
+
+        return $this->render('front/lineup/browse.html.twig', [
+            'lineUpListList' => $lineUpListList,
+        ]);
+    }
+
+    #[Route('/programmation/stage/{id}', name: 'LineUpBystage', methods: ['GET'])]
+    public function lineUpStage(SlotRepository $slotRepository, $id): Response
+    {   
+        $lineUpListList = $slotRepository->findAllSlotWithArtistAndGenreAndStageByStage($id);
+
         return $this->render('front/lineup/browse.html.twig', [
             'lineUpListList' => $lineUpListList,
         ]);
--- src/Repository/SlotRepository.php
+++ src/Repository/SlotRepository.php
@@ -16,7 +16,67 @@ class SlotRepository extends ServiceEntityRepository
         parent::__construct($registry, Slot::class);
     }
 
-    public function findAllSlotWithArtistAndGenre(): array
+    public function findAllSlotWithArtistAndGenreAndStage(): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+        SELECT artist.*,
+        GROUP_CONCAT(genre.name SEPARATOR ", ") AS genres,
+        slot.id AS slot_id,
+        slot.date,
+        stage.name AS stage_name
+        FROM artist
+        INNER JOIN genre_artist 
+        ON artist.id = genre_artist.artist_id
+        INNER JOIN genre 
+        ON genre.id = genre_artist.genre_id
+        INNER JOIN slot 
+        ON artist.id = slot.artist_id
+        INNER JOIN stage 
+        ON stage.id = slot.stage_id
+        GROUP BY artist.id, slot.id
+        ORDER BY artist.name ASC
+            ';
+
+        $resultSet = $conn->executeQuery($sql);
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
+    }
+
+
+    public function findAllSlotWithArtistAndGenreAndStageByDay($date): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+            SELECT artist.*,
+            GROUP_CONCAT(genre.name SEPARATOR ", ") AS genres,
+            slot.id AS slot_id,
+            slot.date,
+            stage.name AS stage_name
+            FROM artist
+            INNER JOIN genre_artist 
+            ON artist.id = genre_artist.artist_id
+            INNER JOIN genre 
+            ON genre.id = genre_artist.genre_id
+            INNER JOIN slot 
+            ON artist.id = slot.artist_id
+            INNER JOIN stage 
+            ON stage.id = slot.stage_id
+            WHERE slot.date = :date
+            GROUP BY artist.id, slot.id
+            ORDER BY artist.name ASC
+            ';
+
+        $resultSet = $conn->executeQuery($sql, ['date' => $date]);
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
+    }
+
+    public function findAllSlotWithArtistAndGenreAndStageByStage($stage): array
     {
         $conn = $this->getEntityManager()->getConnection();
 
@@ -31,9 +91,10 @@ class SlotRepository extends ServiceEntityRepository
             ON artist.id = slot.artist_id
             INNER JOIN stage
             ON stage.id = slot.stage_id
+            WHERE stage.id :stage
             ';
 
-        $resultSet = $conn->executeQuery($sql);
+        $resultSet = $conn->executeQuery($sql, ['stage' => $stage]);
 
         // returns an array of arrays (i.e. a raw data set)
         return $resultSet->fetchAllAssociative();
