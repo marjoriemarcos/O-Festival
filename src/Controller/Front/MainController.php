67a7e9fc93f1434892d3a59bd715f27ec3ac412c
TalyaLALAOUI <talya.lalaoui@gmail.com>
2024-05-23 15:56:34 +0200

    Home slider

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home_front')]
-    public function home_front(): Response
+    public function home_front(ArtistRepository $artistRepository): Response
     {
+        // Récupérer tous les artistes à partir du référentiel
+        $artistList = $artistRepository->findAllArtists();
+
         return $this->render('front/main/home.html.twig', [
             'controller_name' => 'MainController',
+            'artistList' => $artistList
         ]);
     }
 }
--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -16,28 +16,16 @@ class ArtistRepository extends ServiceEntityRepository
         parent::__construct($registry, Artist::class);
     }
 
-//    /**
-//     * @return Artist[] Returns an array of Artist objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Artist
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * Récupère tous les artistes de la base de données.
+     *
+     * @return Artist[]
+     */
+    public function findAllArtists(): array
+    {
+        return $this->createQueryBuilder('a')
+            ->orderBy('a.name', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 }
