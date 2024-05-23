94255be20bb7b17b594c08ac88cbe4a8c32f8935
choulakbadri <choulakbadri@gmail.com>
2024-05-23 14:33:00 +0200

    back genre, slot, stage fixtures integrated

--- src/Controller/Back/GenreController.php
+++ src/Controller/Back/GenreController.php
@@ -5,14 +5,17 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\GenreRepository;
 
 class GenreController extends AbstractController
 {
     #[Route('/back/genre_list', name: 'genreBrowse')]
-    public function genreBrowse(): Response
+    public function genreBrowse(GenreRepository $genreRepository): Response
     {
+        // Fetch genres
+        $genreList = $genreRepository->findAll();
         return $this->render('back/genre/index.html.twig', [
-            'controller_name' => 'GenreController',
+            'genreList' => $genreList,
         ]);
     }
    
--- src/Controller/Back/StageController.php
+++ src/Controller/Back/StageController.php
@@ -5,14 +5,17 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\StageRepository;
 
 class StageController extends AbstractController
 {
     #[Route('/back/stage_list', name: 'stageBrowse')]
-    public function stageBrowse(): Response
+    public function stageBrowse(StageRepository $stageRepository): Response
     {
+        // Fetch stages
+        $stageList = $stageRepository->findAll();
         return $this->render('back/stage/index.html.twig', [
-            'controller_name' => 'StageController',
+            'stageList' => $stageList,
         ]);
     }
 }
