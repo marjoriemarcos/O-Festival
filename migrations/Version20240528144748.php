7605456cf249ee53ee905405923fa86a5fc14877
TalyaLALAOUI <talya.lalaoui@gmail.com>
2024-05-28 17:35:57 +0200

    fixing images

--- migrations/Version20240528144748.php
+++ migrations/Version20240528144748.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240528115308 extends AbstractMigration
+final class Version20240528144748 extends AbstractMigration
 {
     public function getDescription(): string
     {
