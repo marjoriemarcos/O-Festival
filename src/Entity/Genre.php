e529536b69ec009b7b42354b4e084cab95802650
Nicolas Joubert <nicolasjoubert@icloud.com>
2024-05-29 13:42:51 +0200

    feat/message alerte if input is in bdd + close message flash

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -7,8 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: GenreRepository::class)]
+#[UniqueEntity('name')]
 class Genre
 {
     #[ORM\Id]
