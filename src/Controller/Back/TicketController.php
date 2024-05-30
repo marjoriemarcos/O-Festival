9a1e19451c7227fbb647174ea1c4e70a3d8c2871
TalyaLALAOUI <talya.lalaoui@gmail.com>
2024-05-30 12:39:01 +0200

    fix bug

--- src/Controller/Back/TicketController.php
+++ src/Controller/Back/TicketController.php
@@ -15,10 +15,12 @@ use App\Entity\Ticket;
 
 class TicketController extends AbstractController
 {
+
     /**
      * @Route("/back/ticket", name="app_back_ticket_browse", methods={"GET"})
      * Méthode pour afficher tous les billets triés par titre
      */
+    #[Route('/back/ticket', name: 'app_back_ticket_browse', methods: ['GET'])]
     public function browse(TicketRepository $ticketRepository): Response
     {
         // Récupère tous les billets triés par titre
@@ -34,6 +36,7 @@ class TicketController extends AbstractController
      * @Route("/back/ticket/{id<\d+>}", name="app_back_ticket_read", methods={"GET"})
      * Méthode pour afficher les détails d'un billet par son ID
      */
+    #[Route('/back/ticket/{id<\d+>}', name: 'app_back_ticket_read', methods: ['GET'])]
     public function read(int $id, TicketRepository $ticketRepository): Response
     {
         // Récupère le billet par son ID
@@ -54,6 +57,7 @@ class TicketController extends AbstractController
      * @Route("/back/ticket/add", name="app_back_ticket_add", methods={"GET", "POST"})
      * Méthode pour ajouter un nouveau billet
      */
+    #[Route('/back/ticket/add', name: 'app_back_ticket_add', methods: ['GET', 'POST'])]
     public function add(Request $request, EntityManagerInterface $entityManager, TicketRepository $ticketRepository): Response
     {
         // Crée une nouvelle instance de Ticket
@@ -97,6 +101,7 @@ class TicketController extends AbstractController
      * @Route("/back/ticket/{id<\d+>}/edit", name="app_back_ticket_edit", methods={"GET", "POST"})
      * Méthode pour éditer un billet existant
      */
+    #[Route('/back/ticket/{id<\d+>}/edit', name: 'app_back_ticket_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Ticket $ticket, EntityManagerInterface $entityManager, TicketRepository $ticketRepository): Response
     {
         // Crée le formulaire d'édition de billet
@@ -135,6 +140,7 @@ class TicketController extends AbstractController
      * @Route("/back/ticket/{id<\d+>}/delete", name="app_back_ticket_delete", methods={"POST"})
      * Méthode pour supprimer un billet existant
      */
+    #[Route('/back/ticket/{id<\d+>}/delete', name: 'app_back_ticket_delete', methods: ['POST'])]
     public function delete(Request $request, Ticket $ticket, EntityManagerInterface $entityManager): Response
     {
         // Vérifie si le jeton CSRF est valide
@@ -142,14 +148,14 @@ class TicketController extends AbstractController
             // Supprime le billet de la base de données
             $entityManager->remove($ticket);
             $entityManager->flush();
-    
+
             // Ajoute un message flash de succès
             $this->addFlash('success', 'Le billet a été supprimé avec succès.');
         } else {
             // Si le jeton CSRF n'est pas valide, ajoute un message flash d'erreur
             $this->addFlash('error', 'La suppression du billet a échoué. Le jeton CSRF est invalide.');
-        }        
-    
+        }
+
         // Redirige vers la page de navigation des billets
         return $this->redirectToRoute('app_back_ticket_browse');
     }
