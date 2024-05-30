55f4c6ff2f8528170e5245e8225bb6ca3e84d19e
choulakbadri <choulakbadri@gmail.com>
2024-05-30 13:34:18 +0200

    edited the fetchataFromRepo service to reduce the number of dataqueries on home page and programmation page

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,35 +2,27 @@
 
 namespace App\Controller\Front;
 
-use App\Repository\ArtistRepository;
 use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Services\fetchDataFromRepo as ServicesFetchDataFromRepo;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    // Displays the home page
     #[Route('/', name: 'app_main_home')]
     public function home(
         ServicesFetchDataFromRepo $fetchDataFromRepo,
         TicketRepository $ticketRepository
     ): Response {
-        // Fetch all data about artists, stages, genres, and slots
         $data = $fetchDataFromRepo->fetchDataFromRepo();
 
-        // Initialize an array to store ticket durations
         $durations = [24, 48, 72];
-        // Initialize an array to store passes
         $passes = [];
 
-        // Iterate over each duration to fetch corresponding tickets
         foreach ($durations as $duration) {
-            // Fetch tickets for the specified duration
             $tickets = $ticketRepository->findTicketsByDuration($duration);
 
-            // Add tickets to the passes list
             $passes[] = [
                 'data' => $tickets,
                 'title' => 'PASS ' . ($duration / 24) . ' DAY(S)',
@@ -38,7 +30,6 @@ class MainController extends AbstractController
             ];
         }
 
-        // Render the home page view with data
         return $this->render('front/main/home.html.twig', [
             'artistList' => $data['artistList'], 
             'slots' => $data['slotList'],
