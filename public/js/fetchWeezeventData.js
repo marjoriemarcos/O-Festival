ac0bf4cf755216d845a3c8475016374ca54cfda7
choulakbadri <choulakbadri@gmail.com>
2024-06-02 22:13:57 +0200

    script updated

--- public/js/fetchWeezeventData.js
+++ public/js/fetchWeezeventData.js
@@ -1,25 +1,26 @@
 // fetchWeezeventData.js
 
 export function fetchWeezeventData() {
-    fetch("/back/api/weezevent/customers") // Sending a GET request to the Symfony route to fetch data
-        .then(response => response.json()) // Convert the response to JSON
-        .then(data => {
-            // Iterate through participant data and display them in the table
-            const customerTableBody = document.querySelector('.customer-table'); // Select by class
-            customerTableBody.innerHTML = ''; // Clear the table content
-            data.customerList.forEach(customer => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${customer.id_participant}</td>
-                    <td>${customer.owner.first_name}</td>
-                    <td>${customer.owner.last_name}</td>
-                    <td><a href="mailto:${customer.owner.email}">${customer.owner.email}</a></td> 
-                    <td>${data.ticketList[customer.id_ticket]}</td>
-                `;
-                customerTableBody.appendChild(row);
+    if (document.body.classList.contains('weezevent')) {
+        fetch("/back/api/weezevent/customers") // Sending a GET request to the Symfony route to fetch data
+            .then(response => response.json()) // Convert the response to JSON
+            .then(data => {
+                // Iterate through participant data and display them in the table
+                const customerTableBody = document.querySelector('.weezevent-customer-table-body'); // Select the table
+                data.customerList.forEach(customer => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${customer.id_participant}</td>
+                        <td>${customer.owner.first_name}</td>
+                        <td>${customer.owner.last_name}</td>
+                        <td><a href="mailto:${customer.owner.email}">${customer.owner.email}</a></td> 
+                        <td>${data.ticketList[customer.id_ticket]}</td>
+                    `;
+                    customerTableBody.appendChild(row);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
             });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+    }
 }
