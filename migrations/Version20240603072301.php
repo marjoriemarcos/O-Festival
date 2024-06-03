0b6aa3886ba4a65a8e76cee38a128b9195932bd3
Nicolas Joubert <nicolasjoubert@icloud.com>
2024-06-03 09:46:47 +0200

    deconnexion navbar bacl office

--- migrations/Version20240603072301.php
+++ migrations/Version20240603072301.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240602190256 extends AbstractMigration
+final class Version20240603072301 extends AbstractMigration
 {
     public function getDescription(): string
     {
